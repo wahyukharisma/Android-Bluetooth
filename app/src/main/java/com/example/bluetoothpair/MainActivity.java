d50365a5c9a31a7d345be0589bb39b33af2ac824
Wahyu Kharisma <madewahyu39G@gmail.com>
2019-07-11 09:31:45 +0700

    Adding function discoverable
    Make device discoverable

--- app/src/main/java/com/example/bluetoothpair/MainActivity.java
+++ app/src/main/java/com/example/bluetoothpair/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.bluetoothpair;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    // make device discoverable by call this function
+    private void makeDiscoverable(){
+        Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION,300);
+        startActivity(discoverableIntent);
+        Log.d("message","discoverable");
+    }
+
+    
+
+
 }
