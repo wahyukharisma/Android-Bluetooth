597b7aa4b88b37943cf8f008def9b4544b06e151
Wahyu Kharisma <madewahyu39G@gmail.com>
2019-07-11 13:56:42 +0700

    + Create Main UI and initialize
    + Create main ui xml
    + Initialize id in java

--- app/src/main/java/com/example/bluetoothpair/MainActivity.java
+++ app/src/main/java/com/example/bluetoothpair/MainActivity.java
@@ -1,29 +1,40 @@
 package com.example.bluetoothpair;
 
 import android.bluetooth.BluetoothAdapter;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_search,btn_print;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //view ini
+        btn_search  = findViewById(R.id.btn_search);
+        btn_print   = findViewById(R.id.btn_print);
+
+
+        //listener
+        btn_print.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btn_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
-
-
-    // make device discoverable by call this function
-    private void makeDiscoverable(){
-        Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION,300);
-        startActivity(discoverableIntent);
-        Log.d("message","discoverable");
-    }
-
-    
-
 
 }
